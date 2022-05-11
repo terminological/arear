25a6c48215445bdabafd17e8e0c8f75ff2c79443
Rob Challen <rob@terminological.co.uk>
2022-05-11 18:46:24 +0100

    minor changes

--- R/zzz.R
+++ R/zzz.R
@@ -3,18 +3,10 @@ utils::globalVariables(c(".id",".interp","A_U_m_k","D_V_n","R","S_P_m","altCode"
 "intersectionArea","intersectionValue","k","m","n","name","start","supplyCode",
 "tmp_code","tmp_id","tmp_input_id","tmp_name","tmp_output_id","to","to_tmp_id"))
 
-.onAttach = function(libname, pkgname) {
+# paste0(unname(unlist(sapply(c("dplyr","tidyr"), function(p) {
+#   functions = ls(asNamespace("sf"))[ls(asNamespace("sf")) %in% paste0(ls(asNamespace(p)),".sf")]
+#   paste0("#' @importFrom sf ",functions)
+# }))),collapse="\n") %>% clipr::write_clip()
 
-  # This is needed because there are some sf functions that need to be loaded for dplyr syntax to work for sf
-  # objects.
-  mute = lapply(c("dplyr","tidyr"), function(p) {
-
-    functions = ls(asNamespace("sf"))[ls(asNamespace("sf")) %in% paste0(ls(asNamespace(p)),".sf")]
-    suppressWarnings(suppressMessages(
-      library(sf, include.only(paste0("sf::",functions)))
-    ))
-
-  })
-  invisible(NULL)
-
-}
+#' @import sf
+NULL
